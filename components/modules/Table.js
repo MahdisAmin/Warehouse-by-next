ad4cf5f44e7265cd6025f569c1ffd4379b097991
Mahdis Amin <mahdis.amin95@gmail.com>
2024-11-03 23:38:25 +0330

    icon package installed index and table module added

---
+++ components/modules/Table.js
@@ -0,0 +1,40 @@
+import { FaRegEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
+
+function Table({ products, openEditModal, openDeleteModal }) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>نام کالا</th>
+          <th>موجودی</th>
+          <th>قیمت</th>
+          <th>شناسه کالا</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {products?.map((product) => (
+          <tr key={product.id}>
+            <td>{product.name}</td>
+            <td>{product.quantity}</td>
+            <td>{product.price} $</td>
+            <td>{product.id.split("-")}</td>
+            <td></td>
+            <td>
+              <button onClick={() => openEditModal(product)}>
+                <FaRegEdit style={{ color: "green", cursor: "pointer" }} />
+              </button>
+              <button onClick={() => openDeleteModal(product.id)}>
+                <MdDelete style={{ color: "red", cursor: "pointer" }} />
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,81 @@
-import React from 'react'
+import { useState } from "react";
+
+import styles from "../styles/index.module.css";
+
+import { useRoure } from "next/router";
+
+import { useGetAllProducts } from "../services/querie";
+import { deleteCookie } from "../utils/cookie";
+
+import AiOutlineProduct from "react-icons";
 function index() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const { data } = useGetAllProducts(currentPage);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const products = data?.data || [];
+
+  const router = useRoure();
+  const openModel = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModale = () => {
+    setIsModalOpen(false);
+  };
+
+  const logOutHandler = () => {
+    deleteCookie("token");
+    router.push("/login");
+  };
+
+  const addProductsHandler = (newProducts) => {
+    console.log(newProducts);
+  };
+
+  const totalPages = data?.data.totalPages;
+
   return (
-    <div>index</div>
-  )
+    <>
+      <SearchDashboard
+        logOutHandler={logOutHandler}
+        products={products.data}
+        setFilteredProducts={setFilteredProducts}
+      />
+
+      <div className={styles.addContainer}>
+        <div>
+          <p>
+            <AiOutlineProduct
+              style={{ marginLeft: "10px", marginTop: "1px" }}
+            />
+            مدیرت کالا
+          </p>
+        </div>
+        <div>
+          <button onClick={openModel}>افزودن محصول</button>
+        </div>
+      </div>
+      <div className={styles.tableContainer}>
+        <ProductsTable
+          products={filteredProducts}
+          setCurrentPage={setCurrentPage}
+          currentPage={currentPage}
+        />
+      </div>
+      <AddModal
+        isOpen={isModalOpen}
+        onClose={closeModale}
+        onCreat={addProductsHandler}
+      />
+      <Pagination
+        currentPage={currentPage}
+        totalPage={totalPages}
+        onPageChange={setCurrentPage}
+      />
+    </>
+  );
 }
 
-export default index
+export default index;
